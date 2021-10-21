f744336d3afe6a6077ee1db81b0ff1e5fc5441f6
MahsumTarhan7 <68226220+MahsumTarhan7@users.noreply.github.com>
2021-10-21 22:56:29 +0300

    Add files via upload

---
+++ DayExample.java
@@ -0,0 +1,17 @@
+public class DayExample {
+    enum gunler{
+        Pazartesi, Salı, Çarşamba, Perşembe, Cuma, Cumartesi, Pazar
+    };
+
+    public  static void main(String[] args){
+        System.out.println(("Hafatanın"));
+        System.out.println("1. günü: "+ gunler.Pazartesi);
+        System.out.println("2. günü: "+ gunler.Salı);
+        System.out.println("3. günü: "+ gunler.Çarşamba);
+        System.out.println("4. günü: "+ gunler.Perşembe);
+        System.out.println("5. günü: "+ gunler.Cuma);
+        System.out.println("6. günü: "+ gunler.Cumartesi);
+        System.out.println("7. günü: "+ gunler.Pazar);
+
+    }
+}
