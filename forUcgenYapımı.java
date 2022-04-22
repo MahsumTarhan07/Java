5dbb38ec2c09ae61341d80a3aade5f0c1124c157
Mahsum <68226220+MahsumTarhan7@users.noreply.github.com>
2022-04-22 06:44:53 +0300

    Create forUcgenYapımı.java

---
+++ forUcgenYapımı.java
@@ -0,0 +1,17 @@
+
+public class uckenyapımı {
+
+	public static void main(String[] args) {
+
+		for(int ilk = 1; ilk <= 10; ilk++) {
+			
+			for(int son = 1; son<=ilk; son++) {
+				
+				System.out.print("*");
+				
+			}
+			System.out.println(); 
+		}
+	}
+
+}
