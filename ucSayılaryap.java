efda80a9cb19bb56c2bb6e90d32669f63f4a7ff3
Mahsum <68226220+MahsumTarhan7@users.noreply.github.com>
2022-04-22 06:39:23 +0300

    Create ucSayılaryap.java
    Alt alta istediğimiz degerler ile uckenimsi bir piramit yaptık

---
+++ ucSayılaryap.java
@@ -0,0 +1,16 @@
+
+public class tersSayıUcgen {
+
+	public static void main(String[] args) {
+		
+		for(int ustdeger = 10; ustdeger > 0; ustdeger--) {// burda alt alta 1-10 kadar sıralar sayıları
+			
+			for(int altdeger= 1; altdeger<= ustdeger; altdeger++) {//burda ustdeger altdegerden yuksek ise bir bir art dedik
+				
+				System.out.print(altdeger);
+				
+			}
+			System.out.println();
+		}
+	}
+}
